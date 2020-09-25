c87443ff9fb83810291cb9c2b1f121abfe8b547f
kevin <kevincaicedo-liga@hotmail.com>
2020-09-25 15:14:30 -0500

    sigo en login

---
+++ Vista/Html/registro.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registro</title>
+</head>
+<body>
+    
+    <div class="foto_registro">
+        <div class="centar_contenido">
+            <div class="texto">
+                <h2 class="texto_lista">INGRESA TUS DATOS PERSONALES</h2>
+            </div>
+    <div class="formulario">
+        <form action="#" method="POST">
+            <div class="campo">
+                <div class="icon">
+                    <i class="fas fa-user-edit icono"></i>
+                </div>
+                <div class="campo_lista">
+                    <input type="text" class="input_campo" name="nombre" placeholder="Ingresa tu nombre" >
+                    <hr>
+                </div>
+            </div>
+
+            <div class="campo">
+                <div class="icon">
+                    <i class="fas fa-user-tag icono"></i>
+                </div>
+                <div class="campo_lista">
+                    <input type="text" class="input_campo" name="apellido" placeholder="Ingresa tu apellido" >
+                    <hr>
+                </div>
+            </div>
+
+            <div class="campo">
+                <div class="icon">
+                    <i class="fas fa-envelope icono"></i>
+                </div>
+                <div class="campo_lista">
+                    <input type="email" class="input_campo" name="email" placeholder="Ingresa tu email" >
+                    <hr>
+                </div>
+            </div>
+
+            <div class="campo">
+                <div class="icon">
+                    <i class="fas fa-lock icono"></i>
+                </div>
+                <div class="campo_lista">
+                    <input type="password" class="input_campo" name="contrasena" placeholder="Ingresa tu contraseña" >
+                    <hr>
+                </div>
+            </div>
+
+            <div class="submit">
+                <input type="submit" class="input_submit" value="ENVIAR">
+            </div>
+            
+        </form>
+    </div>
+        </div>
+    </div>
+</body>
+</html>
