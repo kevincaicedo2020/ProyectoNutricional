3d6e0edd090f6693ff2b3c43d76ce17594855472
Steven Orbe <stevenorbe416@gmail.com>
2020-10-22 11:26:04 -0500

    tremiando panel 05

--- Vista/Js/proteinasdiarias.js
+++ Vista/Js/proteinasdiarias.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         if(valorReferencia === '1'){
             let sedentario = 0.8;
             let proteinasDiarias;
-            proteinasDiarias = sedentario * peso;
+            proteinasDiarias = Math.round(sedentario * peso);
             console.log("las proteinas diarias a consumir son " + proteinasDiarias + " Gramos ");
             //Mostramos los gramos en la etiqueta span
             var result = document.querySelector("#resul-gramos span");
@@ -14,24 +14,24 @@ $(document).ready(function() {
         } else if(valorReferencia === '2'){
             let atletico = 1.40;
             let proteinasDiarias;
-            proteinasDiarias = atletico * peso;
-            console.log("las proteinas diarias a consumir son " + Math.round(proteinasDiarias) + " Gramos ");
+            proteinasDiarias = Math.round(atletico * peso);
+            console.log("las proteinas diarias a consumir son " + proteinasDiarias + " Gramos ");
             //Mostramos los gramos en la etiqueta span
             var result = document.querySelector("#resul-gramos span");
             result.innerHTML = proteinasDiarias;
         } else if(valorReferencia === '3'){
             let resistencia = 1.10;
             let proteinasDiarias;
-            proteinasDiarias = resistencia * peso;
-            console.log("las proteinas diarias a consumir son " + Math.round(proteinasDiarias) + " Gramos ");
+            proteinasDiarias = Math.round(resistencia * peso);
+            console.log("las proteinas diarias a consumir son " + proteinasDiarias + " Gramos ");
             //Mostramos los gramos en la etiqueta span
             var result = document.querySelector("#resul-gramos span");
             result.innerHTML = proteinasDiarias;
         } else if(valorReferencia === '4'){
             let fuerza = 1.50;
             let proteinasDiarias;
-            proteinasDiarias = fuerza * peso;
-            console.log("las proteinas diarias a consumir son " + Math.round(proteinasDiarias) + " Gramos ");
+            proteinasDiarias = Math.round(fuerza * peso);
+            console.log("las proteinas diarias a consumir son " + proteinasDiarias + " Gramos ");
             //Mostramos los gramos en la etiqueta span
             var result = document.querySelector("#resul-gramos span");
             result.innerHTML = proteinasDiarias;
